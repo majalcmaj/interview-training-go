4d836f08a8a1b3c048a083840bc9443fa7c265e9
Michał Ciesielski <majalcmaj@gmail.com>
2026-09-14 22:00:06 +0200

    Finished 3414 - buggy

--- maxnonoverlapinterval_lc3414.go
+++ maxnonoverlapinterval_lc3414.go
@@ -1,59 +1,61 @@
 package main
 
-type ctx [][]int
+import (
+	"sort"
+)
 
-func (c ctx) maxIntervalsRecursive(idx int, curr []int) ([]int, int) {
-	idxs := make([]int, len(curr)+1)
-	copy(idxs, curr)
-	idxs[len(curr)] = idx
+const MINUS_INF = -1e18
+const MAX_INTERVALS = 4
 
-	bestIdxs := idxs
-	bestTotal := c.sumWeights(idxs)
-
-	if len(idxs) == 4 || idx >= len(c) {
-		return bestIdxs, bestTotal
-	}
-
-	for i := idx + 1; i < len(c); i++ {
-		if c.isNoOverlap(i, idxs) {
-			newBestIdxs, newBestTotal := c.maxIntervalsRecursive(i, idxs)
-			if newBestTotal > bestTotal {
-				bestIdxs = newBestIdxs
-				bestTotal = newBestTotal
-			}
-		}
-	}
-	return bestIdxs, bestTotal
+type interval struct {
+	l, r, weight, idx int
 }
 
-func (c ctx) sumWeights(idxs []int) int {
-	sum := 0
-	for _, idx := range idxs {
-		sum += c[idx][2]
+func maximumWeight(rawIntervals [][]int) []int {
+	intervals := make([]interval, len(rawIntervals))
+	for idx, rawInterval := range rawIntervals {
+		intervals[idx] = interval{rawInterval[0], rawInterval[1], rawInterval[2], idx}
 	}
-	return sum
-}
+	sort.Slice(intervals, func(i, j int) bool { return intervals[i].r < intervals[j].r })
 
-func (c ctx) isNoOverlap(idx int, curr []int) bool {
-	for _, curIdx := range curr {
-		if !(c[idx][0] > c[curIdx][1] || c[curIdx][0] > c[idx][1]) {
-			return false
+	dp := make([][]int, len(rawIntervals)+1)
+	for i := 0; i <= len(rawIntervals); i++ {
+		dp[i] = make([]int, MAX_INTERVALS+1)
+	}
+	for j := 1; j <= MAX_INTERVALS; j++ {
+		dp[0][j] = MINUS_INF
+	}
+
+	biggestNonOverlapIdx := make([]int, len(rawIntervals))
+	for i := 0; i < len(rawIntervals); i++ {
+		biggestNonOverlapIdx[i] = sort.Search(i, func(idx int) bool { return intervals[i].l <= intervals[idx].r })
+	}
+
+	for i := 1; i <= len(rawIntervals); i++ {
+		for j := 1; j <= MAX_INTERVALS; j++ {
+			dp[i][j] = max(dp[biggestNonOverlapIdx[i-1]][j-1]+intervals[i-1].weight, dp[i-1][j])
 		}
 	}
-	return true
-}
 
-func maximumWeight(intervals [][]int) []int {
-	c := ctx(intervals)
-	idxs := make([]int, 0)
-	bestTotal := -1
-	result := []int{}
-	for i := 0; i < len(intervals); i++ {
-		newRes, newTotal := c.maxIntervalsRecursive(i, idxs)
-		if newTotal > bestTotal {
-			result = newRes
-			bestTotal = newTotal
+	maxVal := -1
+	bestJ := -1
+	for j := 1; j <= MAX_INTERVALS; j++ {
+		if dp[len(intervals)][j] > maxVal {
+			bestJ = j
+			maxVal = dp[len(intervals)][j]
 		}
 	}
+	result := make([]int, 0, MAX_INTERVALS)
+
+	for i, j := len(intervals), bestJ; j > 0 && i > 0; {
+		if dp[i][j] == dp[i-1][j] {
+			i = i - 1
+		} else {
+			result = append(result, intervals[i-1].idx)
+			i = biggestNonOverlapIdx[i-1]
+			j = j - 1
+		}
+	}
+	sort.Ints(result)
 	return result
 }
