6dfd753ebdd9987ec15b08d162387f8b33878123
Michał Ciesielski <majalcmaj@gmail.com>
2026-09-15 23:12:57 +0200

    got partial function

--- nooverlappalindromes_lc2472.go
+++ nooverlappalindromes_lc2472.go
@@ -1,5 +1,46 @@
-package main 
+package main
+
+func oddPalindromes(s string, k int) []bool {
+	margin := k / 2
+	dp := make([]bool, len(s))
+
+	for i := margin; i < len(dp)-margin; i++ {
+		dp[i] = true
+	}
+
+	for i := 1; i <= margin; i += 2 { // is this correct? Should be 1?
+		for j := i; j < len(s)-i; j++ {
+			if !(dp[j] && s[j-i] == s[j+i]) {
+				dp[j] = false
+			}
+		}
+	}
+	return dp
+}
+
+func evenPalindromes(s string, k int) []bool {
+	margin := k / 2
+	dp := make([]bool, len(s)-1)
+
+	if k == 1 { // TODO: needed?
+		return dp
+	}
+
+	for i := margin - 1; i < len(dp); i++ {
+		dp[i] = true
+	}
+
+	for i := 0; i <= margin; i++ {
+		for j := i; j < len(s)-i-1; j++ {
+			if !(dp[j] && s[j-i] == s[j+i+1]) {
+				dp[j] = false
+			}
+		}
+	}
+	return dp
+}
 
 func maxPalindromes(s string, k int) int {
-    return 1
+
+	return 1
 }
--- nooverlappalindromes_lc2472_test.go
+++ nooverlappalindromes_lc2472_test.go
@@ -1,6 +1,47 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"slices"
+	"testing"
+)
+
+func TestOddPalindromes(t *testing.T) {
+	for _, d := range []struct {
+		s      string
+		k      int
+		expect []bool
+	}{
+		{"abaccdbbd", 3, []bool{false, true, false, false, false, false, false, false, false}},
+		{"adbcda", 3, []bool{false, false, false, false, false, false}},
+		{"ababa", 5, []bool{false, false, true, false, false}},
+		{"aaaaaa", 3, []bool{false, true, true, true, true, false}},
+	} {
+		res := oddPalindromes(d.s, d.k)
+		if !slices.Equal(res, d.expect) {
+			t.Errorf("Expected %v, got %v for s=%s, k=%d", d.expect, res, d.s, d.k)
+		}
+	}
+}
+
+func TestEvenPalindromes(t *testing.T) {
+	for idx, d := range []struct {
+		s      string
+		k      int
+		expect []bool
+	}{
+		{"abaccdbbd", 4, []bool{false, false, false, false, false, false, true, false}},
+		{"adbcda", 2, []bool{false, false, false, false, false}},
+		{"abaaba", 6, []bool{false, false, true, false, false}},
+		{"aaaaaa", 2, []bool{true, true, true, true, true}},
+	} {
+		fmt.Printf("Running %d\n", idx)
+		res := evenPalindromes(d.s, d.k)
+		if !slices.Equal(res, d.expect) {
+			t.Errorf("Expected %v, got %v for s=%s, k=%d", d.expect, res, d.s, d.k)
+		}
+	}
+}
 
 func TestMaxOverlappingPalindromes(t *testing.T) {
 	for _, d := range []struct {
