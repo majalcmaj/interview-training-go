5b4f8c9dc248b82e3419d0b3cd588cf7280c57f3
Michał Ciesielski <majalcmaj@gmail.com>
2026-09-16 12:44:03 +0200

    (Most probably) working recursive solution

--- nooverlaplinesegments_lc1621.go
+++ nooverlaplinesegments_lc1621.go
@@ -1,5 +1,20 @@
 package main
 
+func _numberOfSets(n int, k int) int64 {
+	// fmt.Printf("n %d k %d\n", n, k)
+	if k == 0 {
+		return 1
+	}
+	if n == 0 {
+		return 0
+	}
+	sum := _numberOfSets(n-1, k)
+	for i := k; i < n; i++ {
+		sum += _numberOfSets(i, k-1)
+	}
+	return sum
+}
+
 func numberOfSets(n int, k int) int {
-	return 0
+	return int(_numberOfSets(n, k) % (1e9 + 7))
 }
