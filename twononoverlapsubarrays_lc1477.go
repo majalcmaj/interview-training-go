c1b6cf77076c3947bd2c8aa982f01e06130ab586
Michał Ciesielski <majalcmaj@gmail.com>
2026-09-17 23:51:46 +0200

    Copied ready solution...

--- twononoverlapsubarrays_lc1477.go
+++ twononoverlapsubarrays_lc1477.go
@@ -1,5 +1,29 @@
 package main
 
 func minSumOfLengths(arr []int, target int) int {
-	return 0
+	pos := map[int]int{0: -1}
+	n := len(arr)
+	s, ans, minL := 0, n+1, n
+	for i, x := range arr {
+		s += x
+		if j, ok := pos[s-target]; ok {
+			length := i - j
+			prev := n
+			if j != -1 {
+				prev = arr[j]
+			}
+			if length+prev < ans {
+				ans = length + prev
+			}
+			if length < minL {
+				minL = length
+			}
+		}
+		arr[i] = minL
+		pos[s] = i
+	}
+	if ans == n+1 {
+		return -1
+	}
+	return ans
 }
